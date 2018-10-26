0e731b441c0a74433324b0e7cf5bbc7679d2146b
andrewchungxam <andrew.chung@xamarin.com>
2018-10-26 11:21:55 -0700

    Created Simplified Echo Bot
    Created Simplified Echo Bot

---
+++ Bot Builder Simplified Echo Bot V4/SimpleEchoBot.cs
@@ -0,0 +1,22 @@
+﻿// Licensed under the MIT License.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+
+namespace Bot_Builder_Simplified_Echo_Bot_V4
+{ 
+    public class SimplifiedEchoBot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext.Activity.Type == ActivityTypes.Message)
+            {
+                var responseMessage1 = $"You sent '{turnContext.Activity.Text}.' \n  The above was called from OnTurnAsync. \n  ";
+                await turnContext.SendActivityAsync(responseMessage1);
+            }
+        }
+    }
+}
