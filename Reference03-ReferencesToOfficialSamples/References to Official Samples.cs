9f1de2a59968b9c9535a99dca3ddc008a8761696
andrewchungxam <andrew.chung@xamarin.com>
2018-11-12 09:38:37 -0500

    continue cleaning code and adding reference code

---
+++ Reference03-ReferencesToOfficialSamples/References to Official Samples.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reference03_ReferencesToOfficialSamples
+{
+    class References_to_Official_Samples
+    {
+    }
+}
