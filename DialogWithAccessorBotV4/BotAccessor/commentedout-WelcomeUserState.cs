9d31cc9ca16a11e4b4c81dfd3bf6ce21762da99d
andrewchungxam <andrew.chung@xamarin.com>
2018-11-12 10:11:14 -0500

    after cleanup 5 dialog with accessor bot

---
+++ DialogWithAccessorBotV4/BotAccessor/commentedout-WelcomeUserState.cs
@@ -0,0 +1,18 @@
+﻿//// Copyright (c) Microsoft Corporation. All rights reserved.
+//// Licensed under the MIT License.
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace DialogWithAccessorBotV4.BotAccessor
+//{
+//    // Stores User Welcome state for the conversation.
+//    // Stored in Microsoft.Bot.Builder.ConversationState and backed by Microsoft.Bot.Builder.MemoryStorage
+//    public class WelcomeUserState
+//    {
+//        /// A bool property --> gets or sets whether the user has been welcomed in the conversation.
+//        public bool DidBotWelcomeUser { get; set; } = false;
+//    }
+//}
